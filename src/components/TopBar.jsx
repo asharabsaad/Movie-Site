efc41c3c0dbceaf8ddcaf8cf96122119f5bd1fa6
Saad <asharab.saad@accenture.com>
2024-08-06 15:46:12 +0530

    first commit

---
+++ src/components/TopBar.jsx
@@ -0,0 +1,22 @@
+export const TopBar = () => {
+    return ( 
+        <div className="flex gap-10 bg-custom-blue text-white">
+            <div className=" absolute right-0">
+                <span>About</span>
+            </div>
+            <div className="absolute right-0">
+                <span>About</span>
+            </div>
+            <div className="absolute right-0">
+                <span>About</span>
+            </div>
+            <div className="absolute right-0">
+                <span>About</span>
+            </div>
+            <div className=" absolute right-0">
+                <span>About</span>
+            </div>
+        </div>
+    );
+  }
+  
